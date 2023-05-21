d5c5c244e70feb8ddbe2e86a0dc48deab2d61832
EkaterinaPashina <k377at@mail.ru>
2023-05-21 23:08:39 +0500

    added comments, right link to avatar image

--- math.js
+++ math.js
@@ -1,6 +1,10 @@
-const button = document.querySelector('button');
+//Определение кнопки отправки
+const button = document.querySelector("button");
 
+//Получение имени пользователя
 let username = document.querySelector("#username");
+
+//Приведение к верному регистру имени пользователя
 username.addEventListener("input", () => {
     let fullName = [];
     let newName = username.value.toLowerCase().split(" ");
@@ -11,23 +15,26 @@ username.addEventListener("input", () => {
     });
 });
 
+//Определение зоны для ввода имени пользователя 
+const printName = document.querySelector(".show__name");
 
-const avatar = document.querySelector('.avatar');
-const image = avatar.value;
+//Определение введенной ссылки аватара
+const img = document.querySelector("#link");
 
-const printName = document.querySelector('.show__name');
-
+//Получение комментария
 const textarea = document.querySelector("#textarea");
+
+//Определение комментария без цензуры
 const dirtyText = textarea.value;
 
+//Создание функции для применения цензуры
 const cleanUp = () => {
     const words = dirtyText.trim().split(' ');
     const cenzoredText = [];
     for (let word of words) {
-        word = word.toLowerCase;
-        if (word === 'viagra') {
+        if (word.toLowerCase === 'viagra') {
             cenzoredText.push('***');
-        } else if (word === 'xxx') {
+        } else if (word.toLowerCase === 'xxx') {
             cenzoredText.push('***')
         } else {
             cenzoredText.push(word);
@@ -37,12 +44,12 @@ const cleanUp = () => {
     return censoredText;
 };
 
-const censoredText = cleanUp();
-
-const click = () => {
-    avatar.setAttribute('src', image);
+//Функция для отображения аватара, имени пользователя, комментария с цензурой
+const showInfo = () => {
+    avatar.setAttribute("src", img.value);
     printName.textContent = username.value;
-    textarea.textContent = censoredText.value;
+    //textarea.textContent = censoredText.value;
 };
 
-button.onclick = click;
+//Определение функции по нажатию кнопки
+button.onclick = showInfo;
