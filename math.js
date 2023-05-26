a5a26cda1d603baa8d09d08616f09829caea2384
EkaterinaPashina <k377at@mail.ru>
2023-05-27 01:29:53 +0500

    added eventlistener and func to clean up comments

--- math.js
+++ math.js
@@ -1,40 +1,58 @@
 //Определение кнопки отправки
 const button = document.querySelector("button");
 
-//Получение имени пользователя
-const username = document.querySelector("#username").value;
-console.log(username);
+//Определение события по нажатию кнопки
+function showChat(event) {
 
-//Приведение к верному регистру имени пользователя
-const rightName = (name) => {
-    const checkedName = name.toLowerCase();
-    result = checkedName[0].toUpperCase() + checkedName.substring(1);
-    return result;
-};
+    //Получение имени пользователя
+    const username = document.querySelector("#username").value;
 
-nameInfo = rightName(username);
-console.log(nameInfo);
+    //Функция приведение к верному регистру имени пользователя
+    const rightName = (name) => {
+        const checkedName = name.toLowerCase();
+        result = checkedName[0].toUpperCase() + checkedName.substring(1);
+        return result;
+    };
 
-//Получение введенной ссылки аватара
-const img = document.querySelector("#link").value;
-console.log(img);
+    //Присвоение правильного значения имени
+    nameInfo = rightName(username);
 
-//Получение комментария
-const textarea = document.querySelector("#textarea").value;
-console.log(textarea);
+    //Получение введенной ссылки аватара
+    const img = document.querySelector("#link").value;
 
-//Функция, которая создает элементы чата
-const showInfo = () => {
+    //Получение комментария
+    const textarea = document.querySelector("#textarea").value;
+
+    //Очищение комментария от стоп-слов
+    const clearComment = (text) => {
+        text = text.replace(/viagra/i, '***');
+        text = text.replace(/xxx/i, '***');
+        return text;
+    };
+
+    //Присвоение правильного значения комментарию
+    const checkedComment = clearComment(textarea);
+
+    //Функция, которая создает элементы чата
     const parent = document.querySelector('#container__chat');
 
-    let p = document.createElement('p');
-    p.textContent = nameInfo;
-    parent.appendChild(p);
+    //Создание элемента имени
+    let name = document.createElement('p');
+    name.textContent = nameInfo;
+    name.className = 'container__chat__name';
+    parent.appendChild(name);
 
-    const image = document.createElement('img');
-    image.src = img;
-    image.className = 'img';
-};
+    //Создание элемента аватара
+    const avatar = new Image();
+    avatar.src = img;
+    avatar.className = 'container__chat__image';
+    parent.appendChild(avatar);
 
-//Определение функции по нажатию кнопки
-button.onclick = showInfo;
+    //Создание элемента комментария
+    let comment = document.createElement('p');
+    comment.textContent = checkedComment;
+    comment.className = 'container__chat__comment';
+    parent.appendChild(comment);
+}
+
+button.addEventListener('click', showChat);
