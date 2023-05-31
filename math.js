712926ecb9f25d63f77811d7e4ff148125343990
EkaterinaPashina <k377at@mail.ru>
2023-05-31 23:14:44 +0500

    added date and img for random avatar

--- math.js
+++ math.js
@@ -34,13 +34,12 @@ function showChat(event) {
     const checkedComment = clearComment(textarea);
 
     //Функция, которая создает элементы чата
-    const parent = document.querySelector('#container__chat');
+    const parent = document.querySelector('.container__user__info');
 
-    //Создание элемента имени
-    let name = document.createElement('p');
-    name.textContent = nameInfo;
-    name.className = 'container__chat__name';
-    parent.appendChild(name);
+    //Создание diva
+    const div = document.createElement('div');
+    div.className = 'container__chat__div';
+    parent.appendChild(div);
 
     //Создание элемента аватара
     const avatar = new Image();
@@ -48,11 +47,33 @@ function showChat(event) {
     avatar.className = 'container__chat__image';
     parent.appendChild(avatar);
 
+    //Создание элемента имени
+    const name = document.createElement('p');
+    name.textContent = nameInfo;
+    name.className = 'container__chat__name';
+    parent.appendChild(name);
+
+    //Добавление даты и времени
+    const now = new Date();
+    const year = now.getFullYear();
+    const month = `${now.getMonth() + 1}`.padStart(2, 0);
+    const date = `${now.getDate()}`.padStart(2, 0);
+    const hours = `${now.getHours()}`.padStart(2, 0);
+    const minutes = `${now.getMinutes()}`.padStart(2, 0);
+    const seconds = `${now.getSeconds()}`.padStart(2, 0);
+    const showDate = `${date}/${month}/${year} ${hours}:${minutes}:${seconds}`;
+
+    const datep = document.createElement('p');
+    datep.textContent = showDate;
+    datep.className = 'container__date';
+    parent.appendChild(datep);
+
     //Создание элемента комментария
     let comment = document.createElement('p');
     comment.textContent = checkedComment;
     comment.className = 'container__chat__comment';
     parent.appendChild(comment);
+
 }
 
 button.addEventListener('click', showChat);
